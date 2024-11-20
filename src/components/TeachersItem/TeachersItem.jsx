57ca936920ad938f53a48e069d71cbea5b5425df
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-11-20 09:35:53 +0200

    teacher item
    add some info

--- src/components/TeachersItem/TeachersItem.jsx
+++ src/components/TeachersItem/TeachersItem.jsx
@@ -10,7 +10,7 @@ const TeachersItem = ({ teacher }) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const dispatch = useDispatch();
    const favourites = useSelector(selectFavourites);
-   const isFavourite = Array.isArray(favourites) && favourites.includes(teacher.id);
+   const isFavourite = Array.isArray(favourites) && favourites.includes(teacher.surname);
 
   const handleToggleFavourite = () => {
     dispatch(toggleFavourite(teacher.surname));
@@ -24,29 +24,36 @@ const TeachersItem = ({ teacher }) => {
             <img className={css.photo} src={teacher.avatar_url} alt={teacher.name} width="96" height="96" />      
           </div>
           <div className={css.info_wrapper}>
-              <div>
-                  <p>Languages:</p>
-                  <span>Lessons online</span>
-                  <p>Lessons done: {teacher.lessons_done}</p>
-                  <p>
-                      <svg width="16" height="16" >
-                           <use href={`${icons}#rating`} />
-                      </svg>
-                      Rating: {teacher.rating}</p>
-                   <p>Price / 1 hour: <span className={css.green}>{teacher.price_per_hour} $</span></p>
-                    <svg
-              width="26"
-              height="26" className={css.heard_icon}
-              onClick={handleToggleFavourite}
-              fill={isFavourite ? "#e44848" : "#101828"}
-              cursor='pointer'>
-              <use href={`${icons}#heard`} />
-            </svg>
+              <div className={css.info}>
+                  <div className={css.teacher_name_wrapper}>
+                    <p className={css.position}>Languages</p>
+                    <p className={css.teacher_name}>{teacher.name} {teacher.surname}</p>
+                  </div>
+                  <div className={css.lessons_info}>
+                    <p> <svg width="16" height="16" >
+                             <use href={`${icons}#book`} />
+                        </svg>Lessons online</p>
+                    <p>Lessons done: {teacher.lessons_done}</p>
+                    <p>
+                        <svg width="16" height="16" >
+                             <use href={`${icons}#rating`} />
+                        </svg>
+                        Rating: {teacher.rating}</p>
+                     <p>Price / 1 hour: <span className={css.green}>{teacher.price_per_hour} $</span></p>
+                      <svg
+                width="26"
+                height="26" className={css.heard_icon}
+                onClick={handleToggleFavourite}
+                fill={isFavourite ? "#e44848" : "none"}
+                cursor='pointer'>
+                <use href={`${icons}#heard`} />
+              </svg>
+                  </div>
               </div>
-              <div>{teacher.name} {teacher.surname}</div>
-              <p>Speaks: <span>{teacher.languages.join(', ')}</span></p>
-              <p>Lesson Info: <span>{teacher.lesson_info}</span></p>
-              <p>Conditions: <span>{teacher.conditions.join(' ')}</span></p>
+              
+              <p className={css.position}>Speaks: <span>{teacher.languages.join(', ')}</span></p>
+              <p className={css.position}>Lesson Info: <span>{teacher.lesson_info}</span></p>
+              <p className={css.position}>Conditions: <span>{teacher.conditions.join(' ')}</span></p>
             
       {isExpanded && (
             <div className="card-body">
